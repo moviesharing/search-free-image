390158563e5de3bf97b653a5304212c21c673d62
tweaksph <tweaksph@gmail.com>
2023-08-27 01:52:05 +0800

    .env update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const accessKey = process.env.JPHABS_KHALIFA_ACCESS_KEY;
+const accessKey = process.env.ACCESS_KEY;
 
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
